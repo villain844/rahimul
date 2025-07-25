ec18ad1693912edefa579446757be2db946c441b
villain844 <samyonosamyoni@gmail.com>
2025-07-26 05:41:23 +0700

    Create dbbg.js

---
+++ database/dbbg.js
@@ -0,0 +1,12 @@
+const backgroundData = [
+  {
+    id: "1",
+    background: "https://files.catbox.moe/uo2ksh.jpeg",
+    status: "nonaktif"
+  },
+  {
+    id: "2",
+    background: "https://files.catbox.moe/0tbu87.png",
+    status: "aktif"
+  }
+];
