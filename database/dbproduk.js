5b04944dfa34846b4f81f9bd840159b0b985ae81
villain844 <samyonosamyoni@gmail.com>
2025-07-26 05:40:38 +0700

    Create dbproduk.js

---
+++ database/dbproduk.js
@@ -0,0 +1,16 @@
+const produkData = [
+  {
+    nama: "ADMIN CH¹",
+    deskripsi: "Akun Admin CH Indo siap pakai | No bug | Stok terbatas!",
+    harga: "Rp 10.000",
+    foto: "https://files.catbox.moe/l53x1u.jpeg",
+    wa: "628123456789"
+  },
+  {
+    nama: "NOKOS 3 JAM",
+    deskripsi: "Nomor kosong 3 jam aktif cocok untuk verifikasi sementara.",
+    harga: "Rp 4.000",
+    foto: "https://files.catbox.moe/yn5xd4.jpeg",
+    wa: "628123456789"
+  }
+];
