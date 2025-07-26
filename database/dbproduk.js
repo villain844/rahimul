f7cc3d9bbd63c942ff32335ccb1d05a60da66c27
villain844 <samyonosamyoni@gmail.com>
2025-07-26 07:12:21 +0700

    Update dbproduk.js

--- database/dbproduk.js
+++ database/dbproduk.js
@@ -1,16 +1,16 @@
-const produkData = [
+const dbproduk = [
   {
     nama: "ADMIN CH¹",
-    deskripsi: "Akun Admin CH Indo siap pakai | No bug | Stok terbatas!",
+    deskripsi: "Akun siap pakai. Fast respon. Dijamin aman dan terpercaya.",
     harga: "Rp 10.000",
-    foto: "https://files.catbox.moe/l53x1u.jpeg",
-    wa: "628123456789"
+    foto: "https://files.catbox.moe/uo2ksh.jpeg",
+    wa: "6281234567890"
   },
   {
-    nama: "NOKOS 3 JAM",
-    deskripsi: "Nomor kosong 3 jam aktif cocok untuk verifikasi sementara.",
+    nama: "NOKOS INDO",
+    deskripsi: "Nomor kosong Indonesia untuk daftar berbagai layanan.",
     harga: "Rp 4.000",
-    foto: "https://files.catbox.moe/yn5xd4.jpeg",
-    wa: "628123456789"
+    foto: "https://files.catbox.moe/0tbu87.png",
+    wa: "6281234567890"
   }
 ];
