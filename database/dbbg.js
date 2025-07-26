cfe8f0d5bd7a295e4b7ff567edadebd9dfc0398e
villain844 <samyonosamyoni@gmail.com>
2025-07-26 07:11:46 +0700

    Update dbbg.js

--- database/dbbg.js
+++ database/dbbg.js
@@ -1,12 +1,4 @@
-const backgroundData = [
-  {
-    id: "1",
-    background: "https://files.catbox.moe/uo2ksh.jpeg",
-    status: "nonaktif"
-  },
-  {
-    id: "2",
-    background: "https://files.catbox.moe/0tbu87.png",
-    status: "aktif"
-  }
+const dbbg = [
+  { id: 1, background: "https://files.catbox.moe/uo2ksh.jpeg", status: "nonaktif" },
+  { id: 2, background: "https://files.catbox.moe/0tbu87.png", status: "aktif" }
 ];
